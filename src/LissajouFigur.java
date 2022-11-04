cafac7ae17a388ef986dbff647bb354c2838b4a1
MaximilianWiesmayr <maxi.wiesmayrr@gmail.com>
2022-11-04 13:07:03 +0100

    added package interface

--- src/LissajouFigur.java
+++ src/LissajouFigur.java
@@ -1,4 +1,6 @@
-public class Main {
+import Interfaces.Components;
+
+public class LissajouFigur implements Components {
     public static void main(String[] args) {
         System.out.println("Hello world");
     }
