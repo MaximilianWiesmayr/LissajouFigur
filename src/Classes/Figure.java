df532faec9b30127a507a819712ceb25c229fb49
szettl135 <56672530+szettl135@users.noreply.github.com>
2022-11-27 18:01:24 +0100

    Made Calculation into a Class

--- src/Classes/Figure.java
+++ src/Classes/Figure.java
@@ -10,11 +10,13 @@ public class Figure extends JPanel implements Globals {
     private int phi;
     private int fx;
     private int fy;
+    private LissajousFigur waveform;
 
     public Figure(int fx, int fy, int phi) {
         this.phi = phi;
         this.fx = fx;
         this.fy = fy;
+        waveform = new LissajousFigur();
     }
 
     @Override
@@ -27,26 +29,15 @@ public class Figure extends JPanel implements Globals {
 
         g.clearRect(0, 0, super.getWidth(), super.getHeight());
         //g.moveTo(cw + (cw - 2) * u(0, fa), ch + (ch - 2) * u(phi, fb)); // Feder zum Startpunkt
-        double xbef = cw + (cw - 2) * u(0, fx);
-        double ybef = ch + (ch - 2) * u(this.phi, fy);
+        double xbef = cw + (cw - 2) * waveform.u(0, fx);
+        double ybef = ch + (ch - 2) * waveform.u(this.phi, fy);
         for (var t = 1; t <= tmax; t++) {
-            double xnew = cw + (cw - 2) * u(t, fx);
-            double ynew = ch + (ch - 2) * u(t + this.phi, fy);
+            double xnew = cw + (cw - 2) * waveform.u(t, fx);
+            double ynew = ch + (ch - 2) * waveform.u(t + this.phi, fy);
             g.drawLine((int) xbef, (int) ybef, (int) xnew,(int)  ynew);
             xbef = xnew;
             ybef = ynew;
         }
-    }
-
-    public double u(int t, int f) {
-        return -Math.sin(t * Math.PI / (180 * f));
-        /*
-        if (-Math.sin(t * Math.PI / (180 * f)) > 0) {
-            return 0.9;
-        }
-        else {
-            return -0.9;
-        }*/
     }
 
     private void paintAxes(Graphics g){
