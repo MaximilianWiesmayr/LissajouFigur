dc6cd8063ecf8661d99ef85e6f6ef3a8a1bddde5
szettl135 <56672530+szettl135@users.noreply.github.com>
2022-11-27 14:14:14 +0100

    Start of Copying code from my Testproject

--- src/Classes/Figure.java
+++ src/Classes/Figure.java
@@ -10,6 +10,7 @@ public class Figure extends JPanel implements Globals {
     private int phi;
     private int fx;
     private int fy;
+
     public Figure(int phi, int fx, int fy) {
         this.phi = phi;
         this.fx = fx;
@@ -18,11 +19,44 @@ public class Figure extends JPanel implements Globals {
 
     @Override
     public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        int fa = 3;
+        int fb = 4;
 
+        int cw = super.getWidth() / 2;
+        int ch = super.getHeight() / 2;
+        int tmax = fa * fb * 360;
+
+        g.clearRect(0, 0, super.getWidth(), super.getHeight());
+        //g.moveTo(cw + (cw - 2) * u(0, fa), ch + (ch - 2) * u(phi, fb)); // Feder zum Startpunkt
+        double xbef = cw + (cw - 2) * u(0, fa);
+        double ybef = ch + (ch - 2) * u(this.phi, fb);
+        for (var t = 1; t <= tmax; t++) {
+            double xnew = cw + (cw - 2) * u(t, fa);
+            double ynew = ch + (ch - 2) * u(t + this.phi, fb);
+            g.drawLine((int) xbef, (int) ybef, (int) xnew,(int)  ynew);
+            xbef = xnew;
+            ybef = ynew;
+        }
+    }
+
+    public double u(int t, int f) {
+        return -Math.sin(t * Math.PI / (180 * f));
+        /*
+        if (-Math.sin(t * Math.PI / (180 * f)) > 0) {
+            return 0.9;
+        }
+        else {
+            return -0.9;
+        }*/
     }
 
     private void paintAxes(Graphics g){
 
+    }
+
+    public void setPhi(int phi){
+        this.phi = phi;
     }
 
     public void changeFX(int newFX){
--- src/LissajouFigur.java
+++ src/LissajouFigur.java
@@ -1,9 +1,18 @@
+import Classes.Animation;
 import Interfaces.Components;
 import Interfaces.Globals;
+import java.awt.BorderLayout;
+import javax.swing.*;
+import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
-public class LissajouFigur implements Components, Globals {
+public class LissajouFigur extends JFrame implements Components, Globals {
 
     public LissajouFigur(){
+        super("Lissa");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(500,500);
+        //add(new Animation());    //statusBar from Components
+        setVisible(true);
 
     }
     public static void main(String[] args) {
