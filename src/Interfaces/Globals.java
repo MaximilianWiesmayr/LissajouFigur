d3da1f464712f7aad8947c903d8b330cbdd86c04
szettl135 <56672530+szettl135@users.noreply.github.com>
2022-11-28 11:53:56 +0100

    Changed the Speedslider to 100 and changed the collor name

--- src/Interfaces/Globals.java
+++ src/Interfaces/Globals.java
@@ -7,9 +7,9 @@ import static java.awt.Font.PLAIN;
 public interface Globals {
     Font LABEL_FONT = new Font("Arial", PLAIN, 12);
 
-    Color COLOR_COORDINATESYSTEM = new Color(0,255,0);
+    Color COLOR_AXES = new Color(0,255,0);
 
-    int RANGE_SPEED = 400;
+    int RANGE_SPEED = 100;
     int RANGE_FXSLIDER = 7;
     int RANGE_FYSLIDER = 7;
 
